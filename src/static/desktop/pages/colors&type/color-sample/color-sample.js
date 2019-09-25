bc7babec98a30272c1b592dd36e82ebd0970617f
Aleksandr Ezersky <smartjsdev@gmail.com>
2019-09-25 16:19:55 +0600

    PUG color-notion refactored

--- src/static/desktop/pages/colors&type/color-sample/color-sample.js
+++ src/static/desktop/pages/colors&type/color-sample/color-sample.js
@@ -1,7 +1,7 @@
-import './__color-notion/_dark-shade-100_1F2041/color-sample__color-notion_dark-shade-100_1F2041';
-import './__color-notion/_dark-shade-075_1F2041/color-sample__color-notion_dark-shade-075_1F2041';
-import './__color-notion/_dark-shade-050_1F2041/color-sample__color-notion_dark-shade-050_1F2041';
-import './__color-notion/_dark-shade-025_1F2041/color-sample__color-notion_dark-shade-025_1F2041';
-import './__color-notion/_dark-shade-005_1F2041/color-sample__color-notion_dark-shade-005_1F2041';
-import './__color-notion/_purple_BC9CFF/color-sample__color-notion_purple_BC9CFF';
-import './__color-notion/_green_6FCF97/color-sample__color-notion_green_6FCF97';
+// import './__color-notion/_dark-shade-100_1F2041/color-sample__color-notion_dark-shade-100_1F2041';
+// import './__color-notion/_dark-shade-075_1F2041/color-sample__color-notion_dark-shade-075_1F2041';
+// import './__color-notion/_dark-shade-050_1F2041/color-sample__color-notion_dark-shade-050_1F2041';
+// import './__color-notion/_dark-shade-025_1F2041/color-sample__color-notion_dark-shade-025_1F2041';
+// import './__color-notion/_dark-shade-005_1F2041/color-sample__color-notion_dark-shade-005_1F2041';
+// import './__color-notion/_purple_BC9CFF/color-sample__color-notion_purple_BC9CFF';
+// import './__color-notion/_green_6FCF97/color-sample__color-notion_green_6FCF97';
