1605f8ea87a4a009cc8b75be973a6f0e31bb37e3
Aleksandr Ezersky <smartjsdev@gmail.com>
2019-10-17 14:19:22 +0600

    Made an active state of the text field. Added the simple jQ dropdown

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,20 @@
 // Main js file
 // see more: https://github.com/vedees/webpack-template/blob/master/README.md#import-js-files
 
+// Plugins 
 import  '../assets/js/jquery-ui';
 import  '../assets/js/jquery.simplePagination';
+import '../assets/css/jquery-ui.min.css';
+import '../assets/css/simplePagination.css';
+
+import '../assets/js/jquery';
+import '../assets/js/jquery.nice-select';
+// import '../assets/scss/nice-select.scss'; =>> main.scss
+
+//Pages
 import './../pages/desktop/pages/colors&type/colors&type';
 import './../pages/desktop/pages/formelements/formelements';
+
+
+//Assets
 import './../assets/scss/utils/fonts.scss';
--- src/pages/desktop/pages/formelements/formelements.js
+++ src/pages/desktop/pages/formelements/formelements.js
@@ -1,5 +1,6 @@
 import './range-slider/range-slider';
 import './pagination/pagination';
+import './dropdown/dropdown';
 
 // var star = document.getElementById('star14');
 // star.setAttribute("checked", true);
