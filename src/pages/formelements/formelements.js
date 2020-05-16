9370abbf6de0b20ed1827145051b826c38e9532f
Aleksandr Ezersky <smartjsdev@gmail.com>
2020-05-16 18:26:22 +0600

    The pages was corrected to compatibility with new blocks and with pages-data options

--- src/pages/formelements/formelements.js
+++ src/pages/formelements/formelements.js
@@ -1,4 +1,4 @@
-// import './blockInput/blockInput';
+import './../../components/input/input';
 import './../../components/dropdown/dropdown';
 import './../../components/rangeSlider/rangeSlider';
 import './../../components/pagination/pagination';
